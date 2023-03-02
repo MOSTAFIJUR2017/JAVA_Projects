24a6fc4103495b971c39f8737478fa6646379fb9
Mostafijur <bhuiyanmostafijur@gmail.com>
2023-03-02 17:27:18 -0500

    Adding the connection details in Conn folder

--- src/electricity/Conn.java
+++ src/electricity/Conn.java
@@ -1,4 +1,27 @@
 package electricity;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
 public class Conn {
+
+    Connection c;
+    Statement s;
+
+    public Conn(){
+      try {
+
+
+          Class.forName("com.mysql.jdbc.Driver");
+          c= DriverManager.getConnection("jdbc:mysql:///ebs","root","Learning2023");
+          s=c.createStatement();
+
+      }catch (Exception e){
+          System.out.println(e);
+
+      }
+
+    }
+
 }
